02f2b731cced464b127043b3389a12886c01f367
Bread-bang <bang960527@gmail.com>
2025-04-09 21:19:55 +0900

    Implementing v1.0

--- ping/README.md
+++ ping/README.md
@@ -3,12 +3,34 @@
 ## Introduction
 
 - ping이란?
-  상대 컴퓨터로 ICMP Echo Request를 전달하여, 네트워크 상태를 판별하는 명령어이다.
+
+      상대 컴퓨터로 ICMP Echo Request를 전달하여, 네트워크 상태를 판별하는 명령어이다.
+
+  참고
+
+  *https://www.rfc-editor.org/rfc/rfc792.html*
+
+  *https://pkg.go.dev/golang.org/x/net/icmp#Message*
+
+- 특징
+
+  1. TCP/UDP를 사용하지 않음
+  2. IP 패킷의 payload는 ICMP message임
 
 ## Roadmap
 
-- v1.0
-  단일 원격 주소에 ICMP Echo Request 전달
+- **v1.0**
 
-- v2.0
-  다중 원격 주소에 ICMP Echo Request 전달
+  단일 원격 주소와의 Ping 통신
+
+- **v2.0**
+
+  다중 원격 주소와의 Ping 통신
+
+- **v3.0**
+
+  raw socket으로 단일 원격 주소와의 Ping 통신
+
+- **v4.0**
+
+  raw socket으로 다중 원격 주소와의 Ping 통신
--- ping/cmd/main.go
+++ ping/cmd/main.go
@@ -1,5 +1,84 @@
 package main
 
+import (
+	"fmt"
+	"net"
+	"os"
+	"time"
+
+	"golang.org/x/net/icmp"
+	"golang.org/x/net/ipv4"
+)
+
 func main() {
-	
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: ./ping1.0v <Destination>")
+		return
+	}
+	target := os.Args[1]
+
+	for {
+		icmpMessage := icmp.Message {
+			Type: ipv4.ICMPTypeEcho, // ICMPTypeEcho = 8
+			Code: 0,
+
+			// Checksum: 
+			//	-> Marshal() 함수로 Checksum을 계산하여 바이트 배열에 포함시킴
+
+			// Identifier, Seq Num, Payload(Data)는 icmp.Echo에 정의 되어 있음
+			Body: &icmp.Echo {
+				ID: os.Getpid() & 0xffff,	// ID는 주로 process id를 적는데, 16바이트로 맞추기 위해 0xffff로 자름
+				Seq: 1,
+				Data: []byte("FirstEcho"), 	// 패킷 손실이나 데이터 변조 여부를 확인하기 위한 문자열
+			},
+		}
+
+		packet, err := icmpMessage.Marshal(nil)
+		if err != nil {
+			fmt.Println("Failed to marshal ICMP:", err)
+			return
+		}
+
+		conn, err := net.Dial("ip4:icmp", target) // arg[0]의 protocol로 arg[1]로 connect 시도
+		if err != nil {
+			fmt.Println("Dial error:", err)
+			return
+		}
+		defer conn.Close()
+
+		if err := conn.SetReadDeadline(time.Now().Add(2 * time.Second)); err != nil {
+			fmt.Println("Deadline error:", err)
+			continue
+		}
+
+		start := time.Now()
+		if _, err := conn.Write(packet); err != nil {
+			fmt.Println("Write error:", err)
+			return
+		}
+
+		reply := make([]byte, 1500)	// 대부분의 Ethernet에서는 MTU가 1500바이트이기 때문
+
+		n, err := conn.Read(reply)
+		if err != nil {
+			fmt.Println("Read error:", err)
+			return
+		}
+
+		duration := time.Since(start)
+		parsedMessage, err := icmp.ParseMessage(1, reply[20:n]) // IP header가 20바이트 이므로
+		if err != nil {
+			fmt.Println("Parse error:", err)
+			return
+		}
+
+		switch parsedMessage.Type {
+		case ipv4.ICMPTypeEchoReply:
+			fmt.Printf("Reply from %s: time=%v\n", target, duration)
+		default:
+			fmt.Printf("Unexpected response: %+v\n", parsedMessage)
+		}
+
+		time.Sleep(1 * time.Second)
+	}
 }
